3f8162840c6e370f0f54e2d545ad98ffd4ad9b27
Ruben Sierra <ruben.sierra@the-cocktail.com>
2012-08-14 10:35:28 +0200

    Rutas para refinery 1.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
-Refinery::Core::Engine.routes.draw do
-  namespace :admin_locales, :path => '' do
-    namespace :admin, :path => 'refinery' do
-      root :to => "admin_locales#index"
+::Refinery::Application.routes.draw do
+  scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
+    scope(:path => 'admin_locales', :as => 'admin_locales', :module => 'admin_locales') do
+      root :to => 'admin_locales#index'
       resources :admin_locales, :only => [:index, :edit, :update]
     end
   end
