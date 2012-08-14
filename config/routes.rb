1d89092e14495be84a7d08aee0bf1ebca65d9532
Ruben Sierra <ruben.sierra@the-cocktail.com>
2012-08-14 14:01:50 +0200

    Corregidas rutas para refinery 1.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
-::Refinery::Application.routes.draw do
+Refinery::Application.routes.draw do
   scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
-    scope(:path => 'admin_locales', :as => 'admin_locales', :module => 'admin_locales') do
-      root :to => 'admin_locales#index'
-      resources :admin_locales, :only => [:index, :edit, :update]
-    end
+    resources :admin_locales, :only => [:index, :edit, :update]
   end
 end
