30d4a9442a81c5a390dd3b7d70a15c1150845740
Ruben Sierra <ruben.sierra@the-cocktail.com>
2012-09-04 12:54:51 +0200

    Gemspec actualizado.

---
+++ lib/refinery/admin_locales/version.rb
@@ -0,0 +1,18 @@
+module Refinery
+  module AdminLocales
+    class Version
+      @major = 2
+      @minor = 1
+      @tiny  = 0
+      @build = 'dev'
+
+      class << self
+        attr_reader :major, :minor, :tiny, :build
+
+        def to_s
+          [@major, @minor, @tiny, @build].compact.join('.')
+        end
+      end
+    end
+  end
+end
