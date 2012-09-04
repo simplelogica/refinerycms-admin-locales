9455528897166b20b10641f5174309a9b9521074
Ruben Sierra <ruben.sierra@the-cocktail.com>
2012-09-04 10:41:40 +0200

    Cambiado el local por defecto en los specs de request.

--- spec/requests/refinery/admin_locales/admin/admin_locales_spec.rb
+++ spec/requests/refinery/admin_locales/admin/admin_locales_spec.rb
@@ -4,8 +4,8 @@ require "spec_helper"
 describe "admin_locales" do
   refinery_login_with :refinery_user
 
-  let(:locale) { 'es' }
-  let(:another_locale) { 'en' }
+  let(:locale) { 'en' }
+  let(:another_locale) { 'es' }
   let(:user) { Refinery::User.first }
   let(:user_with_another_locale) { FactoryGirl.create(:user_with_locale, :locale => another_locale) }
   let(:user_without_locale) { FactoryGirl.create(:user) }
@@ -23,7 +23,7 @@ describe "admin_locales" do
     end
 
     it "shows user with locale locale" do
-      page.should have_xpath("//span[@class='title']//span[@class='preview']", :text => locale)
+      page.should have_xpath("//span[@class='title']//span[@class='preview']", :text => Refinery::I18n.locales[locale.to_sym])
     end
 
     it "shows user with another locale username" do
@@ -31,11 +31,15 @@ describe "admin_locales" do
     end
 
     it "shows user with another locale locale" do
-      page.should have_xpath("//span[@class='title']//span[@class='preview']", :text => another_locale)
+      page.should have_xpath("//span[@class='title']//span[@class='preview']", :text => Refinery::I18n.locales[another_locale.to_sym])
     end
 
     it "shows user without locale username" do
       page.should have_xpath("//span[@class='title']//strong", :text => user_without_locale.username)
+    end
+
+    it "shows user with another locale locale" do
+      page.should have_xpath("//span[@class='title']//span[@class='preview']", :text => 'Locale: Default')
     end
   end
 
